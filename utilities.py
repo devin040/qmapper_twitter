69f529323d91ebb486953a7365882d10123af4d5
Devin Tark <devinvt2@illinois.edu>
2020-10-16 16:22:55 -0500

    Pushing modifed driver code

--- utilities.py
+++ utilities.py
@@ -1,3 +1,9 @@
+import ciso8601
+import time
+def convertTimeString(timestr):
+    ts = ciso8601.parse_datetime(timestr)
+    return time.mktime(ts.timetuple())
+
 def find_ref_tweet_by_id(tweets, id):
     for tweet in tweets:
         if tweet['id'] == id:
@@ -10,3 +16,7 @@ def find_author_by_id(users, id):
         if user['id'] == id:
             return user
     return None
+
+
+print(convertTimeString("2020-10-14T21:29:23.000Z"))
+
