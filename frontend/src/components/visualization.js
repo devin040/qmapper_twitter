7ff58334d8591b5218aa79979f8462df4326ab73
kti3 <71855540+kti3@users.noreply.github.com>
2020-11-17 15:54:29 -0600

    Update visualization.js

--- frontend/src/components/visualization.js
+++ frontend/src/components/visualization.js
@@ -4,66 +4,64 @@ export default Vis;
 
 function Vis(){
 
-var width = 960,
-    height = 500
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-var force = d3.forceSimulation()
-    .force("charge", d3.forceManyBody())
-   
-    .force("center", d3.forceCenter());
+var width = 960;
+var height = 600;
+var svg = d3.select("svg");
+svg.attr("width", width).attr("height", height);
     
-d3.json("d3test2.json", function(json) {
-  force
-      .nodes(json.nodes)
-      .links(json.links)
-      .start();
-
-  var link = svg.selectAll(".link")
-      .data(json.links)
-    .enter().append("line")
+ var label = function(d) {
+    if (d.label === "author") {
+        return "1";
+      }else if (d.label === "tweet") {
+        return "2";
+      }else if (d.label === "context_entity") {
+        return "3";
+      }else{
+        return "4";}
+      }
+    
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+    
+d3.json("d3test2.json", function(error, graph) {
+  if error throw error;
+ 
+  var lines = svg.append("g")
       .attr("class", "link")
+      .selectAll("line")
+      .data(graph.links)
+      .enter().append("line");
       //.style("stroke-width", function(d) { return Math.sqrt(d.weight); });
 
-  var node = svg.selectAll(".node")
-      .data(json.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .call(force.drag);
+  var circles = svg.append("g")
+    .attr("class", "node")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter().append("circle")
+    .attr("r",5)
+    .attr("fill", label);
+      
+ var linkForce = d3.forceLink()
+   .links(graph.links)
+   .id(function(d) {
+       return d.id;
+   });
+    
+  var simulation = d3.forceSimulation(graph.nodes)
+    .force("link", linkForce)
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .on("tick", ticked);
 
-  var label = function(d) {
-    if (d.label === "author") {
-        return "green";
-      }else if (d.label === "tweet") {
-        return "purple";
-      }else if (d.label === "context_entity") {
-        return "red";
-      }else{
-        return "blue";}
-      }
-
-  node.append("circle")
-      .attr("r","5")
-      .style("fill", label)
-      .append("title")
-      .text(function(d) { return d.text; });
-
-  node.append("text")
-      .attr("dx", 12)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name });
-
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
+  function ticked(){
+    circles
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+    lines
+        .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
-    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-  });
-});
+  }
+ }
 }
-
            
