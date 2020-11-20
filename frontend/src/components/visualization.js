14aab2001510f1a5c59bf5aa46ba97c26d7bd306
kti3 <71855540+kti3@users.noreply.github.com>
2020-11-20 13:26:34 -0600

    Update visualization.js

--- frontend/src/components/visualization.js
+++ frontend/src/components/visualization.js
@@ -1,6 +1,6 @@
 import React, {useRef, useEffect} from 'react';
 import * as d3 from "d3";
-export const Vis{
+export const Vis = () => {
     const d3Container = useRef(null);
     useEffect(
         () => {
