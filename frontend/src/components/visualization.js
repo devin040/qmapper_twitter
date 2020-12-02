fa77d81052aa78a9cbc7d3ab9b9dd477f7aad457
kti3 <71855540+kti3@users.noreply.github.com>
2020-12-02 14:05:56 -0600

    Update visualization.js

--- frontend/src/components/visualization.js
+++ frontend/src/components/visualization.js
@@ -1,6 +1,6 @@
 import React, {useRef, useEffect} from 'react';
 import * as d3 from "d3";
-import {event as currentEvent} from 'd3-selection';
+import {event as currentEvent} from 'd3';
 
 
 function Vis() {
